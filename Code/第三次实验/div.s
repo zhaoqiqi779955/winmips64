d6f57fb4c521d32c9b952562b863ad4d9e14c01f
matinjun <44500594+matinjun@users.noreply.github.com>
2020-11-30 17:52:44 +0800

    除法的优化

--- Code/第三次实验/div.s
+++ Code/第三次实验/div.s
@@ -1,30 +1,17 @@
-.data
-a:    .word     12
-b:    .word     3
-c:    .word     15
-d:    .word     5
-e:    .word     1
-f:    .word     2
-g:    .word     3
-h:    .word     4
-i:    .word     5
-      
-      .text
-start:
-      ld r16,a(r0)
-      ld r17,b(r0)
-      ld r18,c(r0)
-      ld r19,d(r0)
-      ddiv r16,r16,r17 #
-      ld r20,e(r0)
-      ld r21,f(r0)
-      ld r22,g(r0)
-      ld r23,h(r0)
-      ddiv r18,r18,r19       #
-      ld r24,i(r0)
-      daddi r20,r20,1
-      daddi r21,r21,1
-      daddi r22,r22,1
-      daddi r23,r23,1
-      daddi r24,r24,1
-      halt
+{
+    "files.autoSave": "off",
+    "julia.executablePath": "D:\\Program_FILE_NEW\\Julia-1.4.2\\bin\\julia.exe",
+    "julia.enableTelemetry": true,
+    "C_Cpp.updateChannel": "Insiders",
+    "julia.environmentPath": "",
+    "go.formatTool": "goimports",
+    "php.validate.executablePath": "D:\\Program_FILE_NEW\\wamp64\\bin\\php\\php7.3.12\\php.exe",
+    "phpformatter.composer": true,
+    "workbench.editorAssociations": [
+        {
+            "viewType": "pdf.preview",
+            "filenamePattern": "*.pdf"
+        }
+    ],
+    "hediet.vscode-drawio.local-storage": "eyIuZHJhd2lvLWNvbmZpZyI6IntcImxhbmd1YWdlXCI6XCJcIixcImN1c3RvbUZvbnRzXCI6W10sXCJsaWJyYXJpZXNcIjpcImdlbmVyYWw7ZGZkO3VtbFwiLFwiY3VzdG9tTGlicmFyaWVzXCI6W1wiTC5zY3JhdGNocGFkXCJdLFwicGx1Z2luc1wiOltdLFwicmVjZW50Q29sb3JzXCI6W10sXCJmb3JtYXRXaWR0aFwiOjI0MCxcImNyZWF0ZVRhcmdldFwiOmZhbHNlLFwicGFnZUZvcm1hdFwiOntcInhcIjowLFwieVwiOjAsXCJ3aWR0aFwiOjgyNyxcImhlaWdodFwiOjExNjl9LFwic2VhcmNoXCI6dHJ1ZSxcInNob3dTdGFydFNjcmVlblwiOnRydWUsXCJncmlkQ29sb3JcIjpcIiNkMGQwZDBcIixcImRhcmtHcmlkQ29sb3JcIjpcIiM2ZTZlNmVcIixcImF1dG9zYXZlXCI6dHJ1ZSxcInJlc2l6ZUltYWdlc1wiOm51bGwsXCJvcGVuQ291bnRlclwiOjAsXCJ2ZXJzaW9uXCI6MTgsXCJ1bml0XCI6MSxcImlzUnVsZXJPblwiOmZhbHNlLFwidWlcIjpcIlwifSJ9"
+}
